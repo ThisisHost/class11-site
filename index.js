09fc270d53fc8c8fb2c7c6abe3b47ab087908ecd
Thisis.host <52318121+pubsite@users.noreply.github.com>
2022-04-14 21:50:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let CEE = new Date(2023,5,7);
-let start = new Date(2021,7,18)
-let day = document.getElementById("day");
-let hour= document.getElementById("hour");
-let min = document.getElementById("min");
-let bar = document.getElementById("bar-line")
+const CEE = new Date(2023,5,7);
+const start = new Date(2020,7,18)
+const day = document.getElementById("day");
+const hour= document.getElementById("hour");
+const min = document.getElementById("min");
+const bar = document.getElementById("bar-line")
 
 update();
     function update() {
@@ -18,7 +18,7 @@ update();
         hour.textContent=hourd+"时"
         min.textContent=mind+"分"
         
-        bar.style.width=Math.ceil(((CEE.getTime()-now.getTime())/(CEE.getTime()-start.getTime())*100))+"%"
+        bar.style.width=Math.ceil(((now.getTime()-start.getTime())/(CEE.getTime()-start.getTime())*100))+"%"
         bar.textContent = "你的高中时代已过去了"+Math.ceil(((now.getTime()-start.getTime())/(CEE.getTime()-start.getTime())*100))+"%";
         setTimeout(update,500)
     }
